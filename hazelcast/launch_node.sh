cf449ba02d7ca1b4bc0c3a9baa3b7bc4e0eaec8f
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-17 23:37:21 +0300

    Added script for launch Hazelcast node

--- hazelcast/launch_node.sh
+++ hazelcast/launch_node.sh
@@ -1,7 +1,7 @@
-MASTER=${1:-hazelcast}
+MASTER=${2:-hazelcast}
 NODE_NAME=""
 
-[[ ! -z "$2" ]] && NODE_NAME="--name $2"
+[[ ! -z "$1" ]] && NODE_NAME="--name $1"
 
 docker run -d ${NODE_NAME} --link ${MASTER}:${MASTER} \
            gangtetris/hazelcast:3.6
