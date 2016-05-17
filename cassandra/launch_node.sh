81d88c66d0f5be7bbe5c8a8beb7b35f3dbbfe200
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-17 23:38:44 +0300

    Removed redundant script for launching Cassandra node

--- cassandra/launch_node.sh
+++
@@ -1,9 +0,0 @@
-MASTER=${1:-cassandra}
-NODE_NAME=""
-
-[[ ! -z "$2" ]] && NODE_NAME="--name $2"
-
-SEED_PROVIDER=$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' ${MASTER})
-docker run -d ${NODE_NAME} -e CASSANDRA_SEEDS="${SEED_PROVIDER}" \
-       gangtetris/cassandra:2.2
-
