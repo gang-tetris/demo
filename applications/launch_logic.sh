0464d44d0db2675bd6c1d2f0730b219a5607e6e8
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-18 20:17:09 +0300

    Respawning logic server on stop

--- applications/launch_logic.sh
+++ applications/launch_logic.sh
@@ -1,6 +1,6 @@
 NUMBER=${1:-}
 
-docker run -d \
+docker run -d --restart=unless-stopped \
     --name logic${NUMBER} -h logic${NUMBER} \
     --link rabbit:rabbit${NUMBER} \
     gangtetris/test-amqp-server
